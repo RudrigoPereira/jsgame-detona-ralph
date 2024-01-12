191221ac2ca294afe0cded0f942a4fcdb8a65dc9
Rodrigo Ferreira Pereira <rudrigopereira@gmail.com>
2024-01-12 00:59:00 -0300

    feat: alguns updates no projeto original para deixar mais com a minha cara

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -4,12 +4,14 @@ const state = {
     enemy: document.querySelector(".enemy"),
     timeLeft: document.querySelector("#time-left"),
     score: document.querySelector("#score"),
+    menuLivesText: document.querySelector(".menu-lives h2"),
   },
   values: {
     gameVelocity: 1000,
     hitPosition: 0,
     result: 0,
     curretTime: 60,
+    lives: 3,
   },
   actions: {
     timerId: setInterval(randomSquare, 1000),
@@ -17,15 +19,38 @@ const state = {
   },
 };
 
-function countDown() {
-  state.values.curretTime--;
+function restartGame() {
+  state.values.result = 0;
+  state.values.curretTime = 60;
+  state.values.lives = 3;
+  state.view.score.textContent = state.values.result;
   state.view.timeLeft.textContent = state.values.curretTime;
+  updateLivesText();
+
+  state.actions.timerId = setInterval(randomSquare, 1000);
+  state.actions.countDownTimerId = setInterval(countDown, 1000);
+}
+
+function countDown() {
+  state.view.timeLeft.textContent = state.values.curretTime;
+  state.values.curretTime--;
+  // state.view.timeLeft.textContent = state.values.curretTime;
 
   if (state.values.curretTime <= 0) {
     clearInterval(state.actions.countDownTimerId);
     clearInterval(state.actions.timerId);
     alert("Game Over! O seu resultado foi: " + state.values.result);
+    restartGame();
   }
+
+  if (state.values.lives <= 0) {
+    alert("Game Over! O seu resultado foi: " + state.values.result);
+    restartGame();
+  }
+}
+
+function updateLivesText() {
+  state.view.menuLivesText.textContent = `x${state.values.lives}`;
 }
 
 function playSound(audioName) {
@@ -53,6 +78,13 @@ function addListenerHitBox() {
         state.view.score.textContent = state.values.result;
         state.values.hitPosition = null;
         playSound("hit");
+      } else {
+        square.classList.add("wrong-click");
+        setTimeout(() => {
+          square.classList.remove("wrong-click");
+        }, 250); 
+        state.values.lives--;
+        updateLivesText();
       }
     });
   });
